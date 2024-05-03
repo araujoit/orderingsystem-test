30a9f39e7703bbd203450501263d7d06d0bbb787
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-05-03 18:22:42 -0300

    Versão simples porém funcional para mensagens simples.

---
+++ src/main/java/br/com/bluesburguer/orderingsystem/test/infra/sqs/SQSConfig.java
@@ -0,0 +1,58 @@
+package br.com.bluesburguer.orderingsystem.test.infra.sqs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class SQSConfig {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+//    @Value("${cloud.aws.credentials.access-key}")
+//    private String accessKeyId;
+//
+//    @Value("${cloud.aws.credentials.secret-key}")
+//    private String secretAccessKey;
+//
+//    @Value("${cloud.aws.end-point.uri}")
+//    private String sqsUrl;
+
+    @Bean
+    @Primary
+    AmazonSQSAsync amazonSQSAsync() {
+    	return AmazonSQSAsyncClientBuilder.standard()
+                //.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(sqsURL, region))
+        		.withRegion(region)
+//                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKeyId, secretAccessKey)))
+                .build();
+    }
+
+    @Bean
+    QueueMessagingTemplate queueMessagingTemplate() {
+        return new QueueMessagingTemplate(amazonSQSAsync());
+    }
+
+    @Bean
+    ObjectMapper objectMapper() {
+        return new ObjectMapper().findAndRegisterModules();
+    }
+    
+    @Bean
+    protected MessageConverter messageConverter(ObjectMapper objectMapper) {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setObjectMapper(objectMapper);
+        converter.setSerializedPayloadClass(String.class);
+        converter.setStrictContentTypeMatch(false);
+        return converter;
+    }
+}
